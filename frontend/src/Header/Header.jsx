26186b641be25dcecc566d7b0c041941550fd8d4
pandawaa <pallakangoding@gmail.com>
2024-06-11 14:00:19 +0800

    Header

---
+++ frontend/src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favorite food here</h2>
+        <p>
+          Choose form diverse menu featuringa delectable array of dishes crafted
+          with the future self
+        </p>
+        <button>Veiw Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
