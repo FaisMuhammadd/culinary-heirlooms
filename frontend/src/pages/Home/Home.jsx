dcf3fea5a4dfad23e74a80f71d57bbaaed16e8c0
pandawaa <pallakangoding@gmail.com>
2024-06-11 16:51:19 +0800

    feat: Food Display

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import Header from "../../Header/Header";
-import ExploreMenu from "../../component/ExploreMenu/ExploreMenu";
+
+import Header from "../../components/Header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import "./Home.css";
+import AppDownload from "../../components/AppDownload/AppDownload";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -10,6 +13,8 @@ const Home = () => {
     <div>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
