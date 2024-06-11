7c125ef393d183e7c8d98991b3fcca33398660a9
pandawaa <pallakangoding@gmail.com>
2024-06-11 16:52:56 +0800

    App Download

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,18 @@
+import { assets } from "../../assets/assets";
+import "./AppDownload.css";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Culinary Heirlooms
+      </p>
+      <div className="app-download-platforms">
+        <img src={assets.play_store} alt="playstore" />
+        <img src={assets.app_store} alt="appstore" />
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
