ae54f3abeed6bf941f9d99e2f05757be2c80f8c1
pandawaa <pallakangoding@gmail.com>
2024-06-20 21:56:20 +0800

    fix server.js

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(port, () => {
-  console.log(`Server Started on localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server Started on localhost:${port}`);
+// });
 
 export default app;
